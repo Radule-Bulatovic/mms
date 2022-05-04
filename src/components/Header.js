90c4981b72ddfc1b83ce30f02c892605508e109d
Radule Bulatovic <radule.bulatovic@quantox.com>
2022-05-04 16:22:45 +0200

    UPD: Header.js to func

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,20 @@
-import React from 'react'
+import React from "react";
 
-export default class Header extends React.Component {
-    render(){
-        return(
-            <header>
-                {/* mmsHeader */}
-                <div className="headerBck mmsHeader">
-                    <img src="logo.png" className="user-image" alt="user" style={{'height':'40px', 'marginBottom':'5px'}} />
-                    {/* <div className="yellowHeader">MMS Podgorica</div> */}
-                </div>
-            </header>
-        )
-    }
-}
+const Header = () => {
+  return (
+    <header>
+      {/* mmsHeader */}
+      <div className="headerBck mmsHeader">
+        <img
+          src="logo.png"
+          className="user-image"
+          alt="user"
+          style={{ height: "40px", marginBottom: "5px" }}
+        />
+        {/* <div className="yellowHeader">MMS Podgorica</div> */}
+      </div>
+    </header>
+  );
+};
+
+export default Header;
