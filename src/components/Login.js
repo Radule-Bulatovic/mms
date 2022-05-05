744c01e4cfa74e8807b7e98f88c2f63589498be0
Radule Bulatovic <radule.bulatovic@quantox.com>
2022-05-05 13:46:57 +0200

    UPD: Login.js to func

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,218 +1,213 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { withRouter } from "react-router-dom";
+import { collapseTextChangeRangesAcrossMultipleVersions } from "typescript";
 import { userPath } from "../constants/path";
 import ModalSuccessOrder from "./ModalSuccessOrder";
 import Loader from "./ui/Loader.tsx";
 // import ModalSuccessOrder from './ModalSuccessOrder'
 
-class Login extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      email: "",
-      password: "",
-      error: "",
-      user: [],
-      isLoading: false,
-      isWritten: false,
+const Login = (props) => {
+  const [email, setemail] = useState("");
+  const [password, setpassword] = useState("");
+  const [error, seterror] = useState("");
+  const [user, setuser] = useState([]);
+  const [isLoading, setisLoading] = useState(false);
+  const [isWritten, setisWritten] = useState(false);
+  const [showModal, setshowModal] = useState(true);
 
-      showModal: false,
-    };
-  }
+  const { error: _error, user: _user } = props;
+  useEffect(() => {
+    resetFields();
+  }, []);
 
-  componentDidMount() {
-    this.resetFields();
-  }
+  useEffect(() => {
+    if (_error?.length > 0 || _error?.length) {
+      setuser([]);
+      setisLoading(false);
+      seterror("Neuspješan login!");
+    }
+  }, [_error]);
 
-  static getDerivedStateFromProps(nextProps, prevProps) {
-    let _user,
-      _isLoading = false,
-      _error;
-    if (
-      nextProps.user !== prevProps.user &&
-      nextProps.user.details !== undefined &&
-      localStorage.getItem("token") !== null
-    ) {
-      if (nextProps.user.details.name.length > 0) {
-        _user = nextProps.user.details;
-        _isLoading = false;
-        _error = "";
+  // static getDerivedStateFromProps(nextProps, prevProps) {
+  //   let _user,
+  //     _isLoading = false,
+  //     _error;
+  //   if (
+  //     nextProps.user !== prevProps.user &&
+  //     nextProps.user.details !== undefined &&
+  //     localStorage.getItem("token") !== null
+  //   ) {
+  //     if (nextProps.user.details.name.length > 0) {
+  //       _user = nextProps.user.details;
+  //       _isLoading = false;
+  //       _error = "";
+  //     }
+  //     return {
+  //       user: _user,
+  //       isLoading: _isLoading,
+  //       error: _error,
+  //     };
+  //   }
+  //   if (nextProps.error !== prevProps.error && nextProps.error !== undefined) {
+  //     if (nextProps.error.length > 0 || prevProps.error.length) {
+  //       _user = [];
+  //       _isLoading = false;
+  //       _error = "Neuspješan login!";
+  //     }
+  //     return {
+  //       user: _user,
+  //       isLoading: _isLoading,
+  //       error: nextProps.error,
+  //     };
+  //   }
+
+  //   if (
+  //     nextProps.isWrittenStoreSurvey &&
+  //     nextProps.isWrittenSchedule &&
+  //     nextProps.isWrittenInvoiceItem
+  //   ) {
+  //     return {
+  //       showModal: true,
+  //     };
+  //   }
+
+  //   return null;
+  // }
+
+  useEffect(() => {
+    if (_user?.details?.name !== undefined) {
+      if (_user.details.name.length > 0) {
+        goToHomePage();
       }
-      return {
-        user: _user,
-        isLoading: _isLoading,
-        error: _error,
-      };
     }
-    if (nextProps.error !== prevProps.error && nextProps.error !== undefined) {
-      if (nextProps.error.length > 0 || prevProps.error.length) {
-        _user = [];
-        _isLoading = false;
-        _error = "Neuspješan login!";
-      }
-      return {
-        user: _user,
-        isLoading: _isLoading,
-        error: nextProps.error,
-      };
-    }
+  }, [_user]);
 
-    if (
-      nextProps.isWrittenStoreSurvey &&
-      nextProps.isWrittenSchedule &&
-      nextProps.isWrittenInvoiceItem
-    ) {
-      return {
-        showModal: true,
-      };
-    }
-
-    return null;
-  }
-
-  componentDidUpdate() {
-    if (this.state.user.name !== undefined) {
-      if (this.state.user.name.length > 0) {
-        this.goToHomePage();
-      }
-    }
-  }
-  closeModal = () => {
-    this.setState({
-      showModal: false,
-    });
-    this.props.resetIsWrittenStore();
-    this.props.resetIsWrittenSchedule();
-    this.props.resetIsWrittenItem();
+  const closeModal = () => {
+    setshowModal(false);
+    props.resetIsWrittenStore();
+    props.resetIsWrittenSchedule();
+    props.resetIsWrittenItem();
   };
 
-  goToHomePage = () => {
+  const goToHomePage = () => {
     let path = userPath.homePage;
-    this.props.history.push(path);
+    props.history.push(path);
   };
 
-  changeEmail = (e) => {
-    this.setState({
-      email: e.target.value,
-      error: "",
-    });
+  const changeEmail = (e) => {
+    setemail(e.target.value);
+    seterror("");
   };
 
-  changePassword = (e) => {
-    this.setState({
-      password: e.target.value,
-      error: "",
-    });
+  const changePassword = (e) => {
+    setpassword(e.target.value);
+    seterror("");
   };
 
-  submitForm = (e) => {
+  const submitForm = (e) => {
     e.preventDefault();
     let credentials = {
-      email: this.state.email,
-      password: this.state.password,
+      email: email,
+      password: password,
     };
-    this.props.login(credentials);
-    // this.resetFields()
-    this.setState({ isLoading: true });
+    props.login(credentials);
+    // resetFields()
+    setisLoading(true);
   };
 
-  resetFields = () => {
+  const resetFields = () => {
     //da bi se onemogucio povratak na login paznju ne treba resetovati user-a
-    this.setState({
-      email: "",
-      password: "",
-      error: "",
-      user: {
-        name: "",
-        operater: "",
-      },
+    setemail("");
+    setpassword("");
+    seterror("");
+    setuser({
+      name: "",
+      operater: "",
     });
   };
 
   // test = () => {
   //     let path = "testing"
-  //     this.props.history.push(path)
+  //     props.history.push(path)
   // }
 
-  render() {
-    return (
-      <div className="card height-style col-lg-5 col-md-12 bck">
-        <article className="card-body">
-          <div className="card-title text-center mb-4 mt-1 setOpacity">
-            <img src="mils-logo.png" className="logo-login" alt="user" />
-          </div>
-          <></>
-          <h5 className="loginError">
-            <span>{this.state.error}</span>
-          </h5>
-          <form
-            name="loginForm"
-            className="setLoginOpacity"
-            onSubmit={this.submitForm}
-          >
-            {this.state.isLoading ? (
-              <Loader />
-            ) : (
-              <div>
-                <div className="form-group">
-                  <div className="input-group">
-                    <div className="input-group-prepend ">
-                      <span className="input-group-text whiteSpan">
-                        {/* <img className="imgStyle" src="boy.png" alt="user"></img> */}
-                        <i className="fa fa-user"></i>
-                      </span>
-                    </div>
-                    <input
-                      className="form-control setFont"
-                      placeholder="E-mail"
-                      type="email"
-                      value={this.state.email || ""}
-                      onChange={this.changeEmail}
-                      required
-                    />
+  return (
+    <div className="card height-style col-lg-5 col-md-12 bck">
+      <article className="card-body">
+        <div className="card-title text-center mb-4 mt-1 setOpacity">
+          <img src="mils-logo.png" className="logo-login" alt="user" />
+        </div>
+        <></>
+        <h5 className="loginError">
+          <span>{error}</span>
+        </h5>
+        <form
+          name="loginForm"
+          className="setLoginOpacity"
+          onSubmit={submitForm}
+        >
+          {isLoading ? (
+            <Loader />
+          ) : (
+            <div>
+              <div className="form-group">
+                <div className="input-group">
+                  <div className="input-group-prepend ">
+                    <span className="input-group-text whiteSpan">
+                      {/* <img className="imgStyle" src="boy.png" alt="user"></img> */}
+                      <i className="fa fa-user"></i>
+                    </span>
                   </div>
-                </div>
-                <div className="form-group">
-                  <div className="input-group">
-                    <div className="input-group-prepend">
-                      <span className="input-group-text whiteSpan">
-                        {/* <img className="imgStyle" src="pass.png" alt="password"></img> */}
-                        <i className="fa fa-lock"></i>
-                      </span>
-                    </div>
-                    <input
-                      className="form-control setFont"
-                      placeholder="********"
-                      type="password"
-                      value={this.state.password || ""}
-                      onChange={this.changePassword}
-                    />
-                  </div>
-                </div>
-
-                <div className="form-group">
-                  <div className="input-group">
-                    <button
-                      className="btn btn-block loginBtn form-control "
-                      type="submit"
-                    >
-                      Login
-                    </button>
-                  </div>
+                  <input
+                    className="form-control setFont"
+                    placeholder="E-mail"
+                    type="email"
+                    value={email || ""}
+                    onChange={changeEmail}
+                    required
+                  />
                 </div>
               </div>
-            )}
-          </form>
-        </article>
+              <div className="form-group">
+                <div className="input-group">
+                  <div className="input-group-prepend">
+                    <span className="input-group-text whiteSpan">
+                      {/* <img className="imgStyle" src="pass.png" alt="password"></img> */}
+                      <i className="fa fa-lock"></i>
+                    </span>
+                  </div>
+                  <input
+                    className="form-control setFont"
+                    placeholder="********"
+                    type="password"
+                    value={password || ""}
+                    onChange={changePassword}
+                  />
+                </div>
+              </div>
 
-        <ModalSuccessOrder
-          sureMessage="Uspjesno ste poslali narudzbu!"
-          showModal={this.state.showModal}
-          closeModal={this.closeModal}
-        />
-      </div>
-    );
-  }
-}
+              <div className="form-group">
+                <div className="input-group">
+                  <button
+                    className="btn btn-block loginBtn form-control "
+                    type="submit"
+                  >
+                    Login
+                  </button>
+                </div>
+              </div>
+            </div>
+          )}
+        </form>
+      </article>
+
+      <ModalSuccessOrder
+        sureMessage="Uspjesno ste poslali narudzbu!"
+        showModal={showModal}
+        closeModal={closeModal}
+      />
+    </div>
+  );
+};
 
 export default withRouter(Login);
