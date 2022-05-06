bc9ca81192290aff1729068fb0168c8376da5e35
Radule Bulatovic <radule.bulatovic@quantox.com>
2022-05-06 11:27:45 +0200

    UPD: LoginCnt.js removed unused imports

--- src/containers/LoginCnt.js
+++ src/containers/LoginCnt.js
@@ -1,9 +1,6 @@
 import { connect } from "react-redux";
 import Login from "../components/Login";
 import { login_request } from "../actions/login.actions";
-import { resetIsWrittenValue } from "../actions/schedule.action";
-import { resetIsWrittenValueStore } from "../actions/storeSurvey.action";
-import { resetIsWrittenValueItem } from "../actions/invoice.action";
 
 const mapStateToProps = (state) => ({
   user: state.loginReducer.user,
