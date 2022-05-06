dc90b7f31b21d487dd324a2d0c910ccae0933f5c
Radule Bulatovic <radule.bulatovic@quantox.com>
2022-05-06 10:34:35 +0200

    UPD: Login.js successModal removed

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,19 +1,13 @@
 import React, { useEffect, useState } from "react";
 import { withRouter } from "react-router-dom";
-import { collapseTextChangeRangesAcrossMultipleVersions } from "typescript";
 import { userPath } from "../constants/path";
-import ModalSuccessOrder from "./ModalSuccessOrder";
 import Loader from "./ui/Loader.tsx";
-// import ModalSuccessOrder from './ModalSuccessOrder'
 
 const Login = (props) => {
   const [email, setemail] = useState("");
   const [password, setpassword] = useState("");
   const [error, seterror] = useState("");
-  const [user, setuser] = useState([]);
   const [isLoading, setisLoading] = useState(false);
-  const [isWritten, setisWritten] = useState(false);
-  const [showModal, setshowModal] = useState(true);
 
   const { error: _error, user: _user } = props;
   useEffect(() => {
@@ -22,57 +16,10 @@ const Login = (props) => {
 
   useEffect(() => {
     if (_error?.length > 0 || _error?.length) {
-      setuser([]);
       setisLoading(false);
       seterror("Neuspješan login!");
     }
   }, [_error]);
-
-  // static getDerivedStateFromProps(nextProps, prevProps) {
-  //   let _user,
-  //     _isLoading = false,
-  //     _error;
-  //   if (
-  //     nextProps.user !== prevProps.user &&
-  //     nextProps.user.details !== undefined &&
-  //     localStorage.getItem("token") !== null
-  //   ) {
-  //     if (nextProps.user.details.name.length > 0) {
-  //       _user = nextProps.user.details;
-  //       _isLoading = false;
-  //       _error = "";
-  //     }
-  //     return {
-  //       user: _user,
-  //       isLoading: _isLoading,
-  //       error: _error,
-  //     };
-  //   }
-  //   if (nextProps.error !== prevProps.error && nextProps.error !== undefined) {
-  //     if (nextProps.error.length > 0 || prevProps.error.length) {
-  //       _user = [];
-  //       _isLoading = false;
-  //       _error = "Neuspješan login!";
-  //     }
-  //     return {
-  //       user: _user,
-  //       isLoading: _isLoading,
-  //       error: nextProps.error,
-  //     };
-  //   }
-
-  //   if (
-  //     nextProps.isWrittenStoreSurvey &&
-  //     nextProps.isWrittenSchedule &&
-  //     nextProps.isWrittenInvoiceItem
-  //   ) {
-  //     return {
-  //       showModal: true,
-  //     };
-  //   }
-
-  //   return null;
-  // }
 
   useEffect(() => {
     if (_user?.details?.name !== undefined) {
@@ -81,13 +28,6 @@ const Login = (props) => {
       }
     }
   }, [_user]);
-
-  const closeModal = () => {
-    setshowModal(false);
-    props.resetIsWrittenStore();
-    props.resetIsWrittenSchedule();
-    props.resetIsWrittenItem();
-  };
 
   const goToHomePage = () => {
     let path = userPath.homePage;
@@ -111,25 +51,14 @@ const Login = (props) => {
       password: password,
     };
     props.login(credentials);
-    // resetFields()
     setisLoading(true);
   };
 
   const resetFields = () => {
-    //da bi se onemogucio povratak na login paznju ne treba resetovati user-a
     setemail("");
     setpassword("");
     seterror("");
-    setuser({
-      name: "",
-      operater: "",
-    });
   };
-
-  // test = () => {
-  //     let path = "testing"
-  //     props.history.push(path)
-  // }
 
   return (
     <div className="card height-style col-lg-5 col-md-12 bck">
@@ -154,7 +83,6 @@ const Login = (props) => {
                 <div className="input-group">
                   <div className="input-group-prepend ">
                     <span className="input-group-text whiteSpan">
-                      {/* <img className="imgStyle" src="boy.png" alt="user"></img> */}
                       <i className="fa fa-user"></i>
                     </span>
                   </div>
@@ -200,12 +128,6 @@ const Login = (props) => {
           )}
         </form>
       </article>
-
-      <ModalSuccessOrder
-        sureMessage="Uspjesno ste poslali narudzbu!"
-        showModal={showModal}
-        closeModal={closeModal}
-      />
     </div>
   );
 };
