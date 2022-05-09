0e4102e4aeac3a387cc5a181c9f510a928ef8491
Radule Bulatovic <radule.bulatovic@quantox.com>
2022-05-09 15:13:39 +0200

    FIX: AnalyticCardItem.js this words removed

--- src/components/singleComponent/AnalyticCardItem.js
+++ src/components/singleComponent/AnalyticCardItem.js
@@ -4,15 +4,9 @@ import moment from "moment";
 
 const AnalyticCardItem = (props) => (
   <tr>
-    <td className="leftAlign">
-      {moment(this.props.date).format("DD-MM-YYYY")}
-    </td>
-    <td className="rightAlign">
-      {this.props.dug === ".00" ? "0.00" : this.props.dug}
-    </td>
-    <td className="rightAlign">
-      {this.props.pot === ".00" ? "0.00" : this.props.pot}
-    </td>
+    <td className="leftAlign">{moment(props.date).format("DD-MM-YYYY")}</td>
+    <td className="rightAlign">{props.dug === ".00" ? "0.00" : props.dug}</td>
+    <td className="rightAlign">{props.pot === ".00" ? "0.00" : props.pot}</td>
   </tr>
 );
 
